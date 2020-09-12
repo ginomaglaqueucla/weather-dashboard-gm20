66ce3c76ebfe3bf9ec7773d695bd1abc64f47377
ginomaglaque <ginomaglaque@gmail.com>
2020-09-12 10:45:45 -0700

    save city to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,38 @@
 var userFormEl = document.querySelector("#user-form");
 var searchTermEl = document.querySelector("#search-term");
 
+// array of previous cities entered
+cityHistory = [];
+
+var loadCityHistory = function() {
+    cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    if(!cityHistory){
+        console.log("I'm empty");
+        // this may be redundent
+        cityHistory = [];
+    }
+}
+
+var saveCityHistory = function() {
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+}
+
 // handles when submission
 var formSubmitHandler = function(event) {
     event.preventDefault();
     console.log("hello");
 
+    // grab user input
     var searchTerm = searchTermEl.value.trim();
-    console.log(searchTerm);
+    // push to array
+    cityHistory.push(searchTerm);
+    console.log(cityHistory);
+    // save local storage
+    saveCityHistory();
+
+
 }
 
 
-
+loadCityHistory();
 userFormEl.addEventListener("submit", formSubmitHandler);
